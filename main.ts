3643693d5d401b854ee133304bc632ad43ef4446
Mauricio Morales T <m.morales9@uniandes.edu.co>
2023-05-28 22:21:45 -0500

    Metodos

--- main.ts
+++ main.ts
@@ -6,6 +6,17 @@ import { Plataforma } from "./plataforma.js";
 import { Plan } from "./plan.js";
 
 
+const series = [];
+const obj1= new SerieTv("x","prueba",["comedia"]);
+const obj2= new SerieTv("y","pruebay",["terror"]);
+
+
+series.push(obj1.nombre);
+series.push(obj2.nombre);
+
+
+console.log(series);
+
 
 
 
